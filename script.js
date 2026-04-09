bf531664c302b5650a96118dfa8beb86899dfd32
lucky-strikealfa <atepmims@gmail.com>
2026-04-09 15:52:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ document.getElementById("form").addEventListener("submit", function(e){
   e.preventDefault();
   document.getElementById("msg").innerText = "Pesan berhasil dikirim!";
 });
+
+let index = 0;
+const slides = document.querySelector(".slides");
+const totalSlides = document.querySelectorAll(".slide").length;
+
+// AUTO SLIDE
+setInterval(() => {
+  nextSlide();
+}, 4000);
+
+function nextSlide(){
+  index++;
+  if(index >= totalSlides) index = 0;
+  updateSlide();
+}
+
+function prevSlide(){
+  index--;
+  if(index < 0) index = totalSlides - 1;
+  updateSlide();
+}
+
+function updateSlide(){
+  slides.style.transform = "translateX(-" + (index * 100) + "%)";
+}
+
+// BUTTON
+document.querySelector(".next").onclick = nextSlide;
+document.querySelector(".prev").onclick = prevSlide;
