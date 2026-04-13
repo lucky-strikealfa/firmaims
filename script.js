5b560a24aabbb81ea8053370d9576143317b9d0d
lucky-strikealfa <atepmims@gmail.com>
2026-04-13 09:20:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,27 @@ slider.addEventListener("mouseleave", start);
 
 // INIT
 showSlide();
+
+
+// ================= MENU HAMBURGER =================
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+
+// ================= ANIMASI SCROLL =================
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".card, .tentang, form, h2").forEach(el => {
+  el.classList.add("hidden");
+  observer.observe(el);
+});
