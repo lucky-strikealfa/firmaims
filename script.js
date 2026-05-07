b0bc500ba548acd6ca5e6c1c84def5729e2034a5
lucky-strikealfa <atepmims@gmail.com>
2026-05-07 16:32:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -162,3 +162,16 @@ window.addEventListener("scroll", () => {
   });
 
 });
+
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  reveals.forEach(reveal => {
+    const top = reveal.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if(top < windowHeight - 100){
+      reveal.classList.add('active');
+    }
+  });
+});
