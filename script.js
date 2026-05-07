45e1ede843fb444660becd0cb0461097c618489d
lucky-strikealfa <atepmims@gmail.com>
2026-05-07 14:36:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -133,3 +133,32 @@ window.addEventListener("scroll", () => {
   });
 
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 120;
+
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(a => {
+
+    a.classList.remove("active");
+
+    if(a.getAttribute("href") === `#${current}`){
+      a.classList.add("active");
+    }
+
+  });
+
+});
